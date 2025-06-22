a7e1c123dab0574e1b7cc8623b06b491ac54ed65
devchi101 <chichichi3100@gmail.com>
2025-06-22 14:03:23 +0100

    Update broadcast.js

--- netlify/functions/broadcast.js
+++ netlify/functions/broadcast.js
@@ -1,26 +1,18 @@
-const { Connection } = require("@solana/web3.js");
+const { Connection } = require('@solana/web3.js');
+const bs58 = require('bs58');
 
 exports.handler = async function (event) {
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: "Only POST allowed",
-    };
-  }
-
   try {
-    const { rawTx } = JSON.parse(event.body);
-    if (!rawTx) throw new Error("Missing raw transaction");
+    const { signedTxBase64 } = JSON.parse(event.body);
+    const connection = new Connection(process.env.QUICKNODE_RPC, 'confirmed');
 
-    const connection = new Connection(process.env.QUICKNODE_RPC, "confirmed");
-
-    const txBuffer = Buffer.from(rawTx); // rawTx is expected as array of bytes
-    const signature = await connection.sendRawTransaction(txBuffer);
-    await connection.confirmTransaction(signature, "finalized");
+    const rawTx = Buffer.from(signedTxBase64, 'base64');
+    const txId = await connection.sendRawTransaction(rawTx);
+    await connection.confirmTransaction(txId, 'finalized');
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ signature }),
+      body: JSON.stringify({ signature: txId }),
     };
   } catch (err) {
     console.error("Broadcast error:", err);
